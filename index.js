1d7e41c7b7e5a104b2bc075feebdcf2e367bcb14
Roger Enand <rogerenand@gmail.com>
2020-11-27 00:22:41 -0800

    got mentions working

--- index.js
+++ index.js
@@ -7,7 +7,42 @@ const web = new WebClient(process.env.SLACK_TOKEN);
 const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
 const currentTime = new Date().toTimeString();
 const slackEvents = createEventAdapter(slackSigningSecret);
+const port = 3000;
 // sendMessage('bot',`The current time is ${currentTime}`);
+startEvents();
+
+
+slackEvents.on('app_mention', (event) => {
+  let mention = event.text.slice(15);
+  mention = mention.split(' ');
+  mention.slice(0,1);
+  switch (mention[0]){
+    case 'hi':
+      sendMessage('bot', `Thanks for saying hi.`);
+      break;
+    case 'help':
+      sendMessage('bot', `Need help?`);
+      break;
+    default:
+      sendMessage('bot', `I repeat, ${mention.join(' ')}`);
+      break;
+  }
+});
+slackEvents.on('error', (error) => {
+  console.log(error.name); // TypeError
+});
+
+function onHi(){
+
+}
+
+async function startEvents(){
+  // Start the built-in server
+  const server = await slackEvents.start(port);
+
+  // Log a message when the server is ready
+  console.log(`Listening for events on ${server.address().port}`);
+};
 
 async function sendMessage(channel, message){
 
